9d27a0aee265c8d2aa7bc391a7a3b332fc4c1054
Vlad20 <158486051+glemiu6@users.noreply.github.com>
2025-05-07 01:18:00 +0100

    Update task 10.sql

--- task 10.sql
+++ task 10.sql
@@ -1,3 +1,4 @@
+-- "Show pruducts per categories {Cheap / Normal / Expensive} where Cheap < 30 and Expensive > 80 *** provide 2 way to get the result"
 SELECT
 	CASE 
 		WHEN ListPrice < 30 THEN 'Cheap'
@@ -10,7 +11,18 @@ FROM Product
 GROUP BY 	
 	CASE
 		WHEN ListPrice < 30 THEN 'Cheap' 
-		WHEN ListPrice> 80 then 'Expensive' 
+		WHEN ListPrice > 80 then 'Expensive' 
 		ELSE 'Normal'
-		END ;
-	
+	END ;
+
+SELECT 
+sum(CASE WHEN ListPrice<30 THEN 1 ELSE 0 END) as Cheap,
+sum(CASE WHEN ListPrice>=30 and ListPrice<8=0 then 1 ELSE 0 END) as Normal,
+sum(CASE WHEN ListPrice>80 THEN 1 ELSE 0 END) as Expensive
+FROM Product
+GROUP BY 
+CASE 
+WHEN ListPrice< 30 THEN 'Cheap'
+WHEN ListPrice>=30 and ListPrice<=80 THEN 'Normal'
+ELSE 'Expensive'
+END;
