753f909579d506e4b6b02bb23500d925ad00fac1
Vlad20 <158486051+glemiu6@users.noreply.github.com>
2025-05-07 01:05:10 +0100

    Update task 5.sql

--- task 5.sql
+++ task 5.sql
@@ -1,3 +1,4 @@
-SELECT count(Size) as no_size FROM Product
--- Sa revad cand pot de ce nu merge cum trebuie
+--Show how many products do not have a size
+SELECT count(*) as count_size FROM Product
+WHERE  Size is NULL 
 
