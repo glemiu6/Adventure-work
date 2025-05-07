8735ffc59980edf40098928d140431ea990775d2
Vlad20 <158486051+glemiu6@users.noreply.github.com>
2025-05-07 01:08:01 +0100

    Update task 9.sql

--- task 9.sql
+++ task 9.sql
@@ -1,10 +1,11 @@
---SELECT  
---sum(CASE 
---WHEN ListPrice < 40.0 THEN 1 ELSE 0 END) as Cheap_products,
---sum(
---CASE 
---WHEN ListPrice >40.0 THEN 1 ELSE 0 END) as Expensive_products
---FROM Product;
+-- "Show how many cheap and expensive products are 
+-- (Cheap means ListPrice < $40)*** provide 2 way to get the result"
+SELECT  
+sum(CASE 
+WHEN ListPrice < 40.0 THEN 1 ELSE 0 END) as Cheap_products,
+sum(CASE 
+WHEN ListPrice >40.0 THEN 1 ELSE 0 END) as Expensive_products
+FROM Product;
 
 SELECT
 CASE WHEN ListPrice<40 THEN 'Cheap' ELSE 'Expensive' END as Category,
