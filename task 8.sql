2f1c8c08e4d187f86e492d79ee0fdd51b42c64ba
Vlad20 <158486051+glemiu6@users.noreply.github.com>
2025-05-07 01:06:52 +0100

    Update task 8.sql

--- task 8.sql
+++ task 8.sql
@@ -1,3 +1,4 @@
+-- Show how many products have no image available
 SELECT count(Product.ThumbnailPhotoFileName) as no_foto
 FROM Product
 WHERE ThumbnailPhotoFileName like '%no_image_available_small.gif%'
